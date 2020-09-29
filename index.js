29bfd5952164d8df8dcbbc90c8332aa7f603d0ca
pkbhowmick <pkbhowmick007@gmail.com>
2020-09-30 00:08:48 +0600

    feat: Create a tasklist app with get and post method

---
+++ index.js
@@ -0,0 +1,48 @@
+const Joi = require('joi')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+taskList = [
+    {id:1,task:'task1'},
+    {id:2,task:'task2'}
+]
+
+app.get('/', (req,res) =>{
+    res.send('Hello world!');
+});
+
+
+app.get('/api/tasks', (req,res)=>{
+    res.send(taskList);
+});
+
+app.get('/api/tasks/:id',(req,res)=>{
+    const task = taskList.find(c => c.id === parseInt(req.params.id));
+    if(!task) return res.status(404).send('Found no entry')
+
+    return res.send(task);
+})
+
+app.post('/api/tasks',(req,res)=>{
+    const schema =Joi.object ({
+        task : Joi.string().required()
+    });
+
+    const result = schema.validate(req.body);
+
+    if(result.error){
+        return res.status(400).send(result.error.details[0].message);
+    }
+    const task = {
+        id: taskList.length+1,
+        task: req.body.task
+    };
+    taskList.push(task);
+    return res.send(task);
+})
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>console.log(`Listening on port ${port}`));
