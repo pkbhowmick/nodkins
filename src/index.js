97fecb2e16006f0bda4835e3624691f1bf3c7eb1
pkbhowmick <pkbhowmick007@gmail.com>
2020-10-03 00:10:54 +0600

    Add unit test for post api

--- src/index.js
+++ src/index.js
@@ -7,23 +7,26 @@ taskList = [
 ]
 
 const taskRouter = require("./routes/task")
+const { config } = require('chai')
 
-app.use((req,res,next) => {
-    console.log(`${new Date().toString()} => ${req.originalUrl}`);
-    next();
-})
+if (process.env.NODE_ENV != 'test') {
+    app.use((req, res, next) => {
+        console.log(`${new Date().toString()} => ${req.originalUrl}`);
+        next();
+    })
+}
 
 app.use(taskRouter);
 
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
 
 
 const port = process.env.PORT || 3000
-app.listen(port, ()=>console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
--- test/task.test.js
+++ test/task.test.js
@@ -1,3 +1,4 @@
+process.env.NODE_ENV = 'test';
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const should = chai.should()
@@ -12,6 +13,22 @@ describe('/GET task',()=>{
         .end((err,res)=>{
             res.should.have.status(200);
             res.body.should.be.a('array');
+        done();
+        })
+    })
+})
+
+describe('/POST task',()=>{
+    it('It should POST a task', (done)=>{
+        let task = {"task":"testTask"}
+        chai.request(server)
+        .post('/api/tasks')
+        .send(task)
+        .end((err,res)=>{
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('id');
+            res.body.should.have.property('task');
         done();
         })
     })
